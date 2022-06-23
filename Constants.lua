8f32ae55f809aebfb66d661146954c06027915d4
cyrus01337 <cyrus21337@gmail.com>
2022-06-23 01:54:03 +0100

    Refine strip regex

--- Constants.lua
+++ Constants.lua
@@ -1,5 +1,5 @@
 return {
-    STRIP_PATTERN = "^%s*(%S?.*%S+)%s*$",
+    STRIP_REGEX = "^%s*(%S?.*%S+)%s*$",
     URL_REGEX = "^https?://[%p%w]+$",
     ABBREVIATIONS = {"K", "M", "B", "T", "Qa", "Qi", "Sx", "Sp", "Oc", "No", "De"}
 }
