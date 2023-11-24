51623602da2eaf56eb8a33e036de2b3ded52df55
cyrus01337 <cyrus21337@gmail.com>
2023-11-24 06:53:56 +0000

    Oh my goodness

--- Types.lua
+++ Types.lua
@@ -1,5 +1,5 @@
 export type Record<K = string, V = any> = { [K]: V }
 export type Array<T = any> = { [number]: T }
-export type Table = Types.Record | Types.Array
+export type Table = Record | Array
 
 return {}
