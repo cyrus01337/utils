a811544f67e3435ebfcc4a93302ed3a06522c252
cyrus01337 <cyrus21337@gmail.com>
2024-02-03 10:01:32 +0000

    Add typecasting with runtime types (this is awful)

--- Types.lua
+++ Types.lua
@@ -2,4 +2,19 @@ export type Table<V = any, K = any> = { [K]: V }
 export type Record<K = string, V = any> = Table<V, K>
 export type Array<V = any> = Table<V, number>
 
-return {}
+local TableLiteral: Table = {}
+local RecordLiteral: Record = {}
+local ArrayLiteral: Array = {}
+local Types = {
+	Number = 0,
+	String = "",
+	Table = TableLiteral,
+	Record = RecordLiteral,
+	Array = ArrayLiteral,
+}
+
+function Types.cast<T>(from: any, _: T): T
+	return from
+end
+
+return Types
