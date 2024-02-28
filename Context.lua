4a4d4e04b0b1a3c7a0fff52443ba4f9b50f17aae
cyrus01337 <cyrus21337@gmail.com>
2024-02-28 14:55:47 +0000

    Improve readability via generic type inference

--- Context.lua
+++ Context.lua
@@ -1,13 +1,12 @@
 --!strict
 local TableUtils = require(script.Parent.Table)
-local Types = require(script.Parent.Types)
 
 local Context = {}
 
-type Context = Types.Table
-export type Type = Context
+type Context<T> = T
+export type Type<T> = Context<T>
 
-function Context.create<Context>(defaults: Context): Context
+function Context.create<Defaults>(defaults: Defaults): Context<Defaults>
     return TableUtils.deepCopy(defaults)
 end
 
